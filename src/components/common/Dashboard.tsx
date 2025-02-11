fabd47352f149e0e3a7b40ba6fc7aad13f67bd98
unknown <adrianapaivausa@icloud.com>
2025-02-11 17:56:12 -0500

    added glassmorphism to dashboard and img on the background

--- src/components/common/Dashboard.tsx
+++ src/components/common/Dashboard.tsx
@@ -1,9 +1,18 @@
 import { PropsWithChildren } from 'react'
+import flowers from '../../assets/images/13.png'
 
 const Dashboard: React.FC<PropsWithChildren<{}>> = ({ children }) => {
     return (
         <>
-        <div className="rounded-4xl bg-background2 container relative z-10">{children}</div>
+        <div className="relative">
+                <img 
+                    src={flowers} 
+                    alt="flowers" 
+                    className="absolute top-130 sm:top-110 md:top-90 lg:top-60 xl:top-10 object-contain mix-blend-multiply"
+                />
+            </div>
+            
+        <div className="rounded-4xl bg-background2/20 backdrop-blur-sm container relative z-10 border border-white/20 shadow-lg">{children}</div>
         </>
     )
 }
